ac03f8358d1c841d26511a604dd6283ce7bc5f70
Alain Schlesser <alain.schlesser@gmail.com>
2017-07-18 19:47:38 +0200

    `maybe_localize()` is called twice. Resolves #4

--- src/DependencyManager.php
+++ src/DependencyManager.php
@@ -339,7 +339,9 @@ class DependencyManager implements DependencyManagerInterface {
 	 *                          'dependency_type'.
 	 */
 	protected function maybe_localize( $dependency, $context ) {
-		if ( ! array_key_exists( 'localize', $dependency ) ) {
+		static $already_localized = [];
+		if ( ! array_key_exists( 'localize', $dependency )
+		     || array_key_exists( $dependency['handle'], $already_localized ) ) {
 			return;
 		}
 
@@ -350,6 +352,7 @@ class DependencyManager implements DependencyManagerInterface {
 		}
 
 		wp_localize_script( $dependency['handle'], $localize['name'], $data );
+		$already_localized[ $dependency['handle'] ] = true;
 	}
 
 	/**
